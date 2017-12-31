5c0db02498ad97a6d95605370a46a9a0cf72083e
Daniel Murtha <Demonslyr@gmail.com>
2017-12-30 20:19:13 -0600

    included arduino helper sketch

---
+++ ArduinoCompanion/ArduinoCompanion.ino
@@ -0,0 +1,173 @@
+// Program used to test the USB Joystick object on the 
+// Arduino Leonardo or Arduino Micro.
+//
+// Matthew Heironimus
+// 2015-03-28 - Original Version
+// 2015-11-18 - Updated to use the new Joystick library 
+//              written for Arduino IDE Version 1.6.6 and
+//              above.
+// 2016-05-13   Updated to use new dynamic Joystick library
+//              that can be customized.
+//------------------------------------------------------------
+
+#include "Joystick.h"
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x40
+int number = 0;
+int state = 0;
+// Create Joystick
+Joystick_ Joystick;
+
+// Set to true to test "Auto Send" mode or false to test "Manual Send" mode.
+//const bool testAutoSendMode = true;
+const bool testAutoSendMode = false;
+
+const unsigned long gcCycleDelta = 1000;
+const unsigned long gcAnalogDelta = 25;
+const unsigned long gcButtonDelta = 500;
+unsigned long gNextTime = 0;
+unsigned int gCurrentStep = 0;
+
+void testXYAxis(unsigned int currentStep)
+{
+  int xAxis;
+  int yAxis;
+  
+  if (currentStep < 256)
+  {
+    xAxis = currentStep - 127;
+    yAxis = -127;
+    Joystick.setXAxis(xAxis);
+    Joystick.setYAxis(yAxis);
+  } 
+  else if (currentStep < 512)
+  {
+    yAxis = currentStep - 256 - 127;
+    Joystick.setYAxis(yAxis);
+  }
+  else if (currentStep < 768)
+  {
+    xAxis = 128 - (currentStep - 512);
+    Joystick.setXAxis(xAxis);
+  }
+  else if (currentStep < 1024)
+  {
+    yAxis = 128 - (currentStep - 768);
+    Joystick.setYAxis(yAxis);
+  }
+  else if (currentStep < 1024 + 128)
+  {
+    xAxis = currentStep - 1024 - 127;
+    Joystick.setXAxis(xAxis);
+    Joystick.setYAxis(xAxis);
+  }
+}
+
+void testThrottleRudder(unsigned int value)
+{
+  Joystick.setThrottle(value);
+  Joystick.setRudder(value);
+}
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+  
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+  
+  Serial.println("Ready!");
+
+  // Set Range Values
+  //Joystick.setXAxisRange(-127, 127);
+  Joystick.setXAxisRange(54, 150);
+  //Joystick.setThrottleRange(0, 255);
+  
+  if (testAutoSendMode)
+  {
+    Joystick.begin();
+  }
+  else
+  {
+    Joystick.begin(false);
+  }
+    // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+delay(5);
+//  // System Disabled
+//  if (digitalRead(A0) != 0)
+//  {
+//    // Turn indicator light off.
+//    digitalWrite(13, 0);
+//    return;
+//  }
+
+  // Turn indicator light on.
+  digitalWrite(13, 1);
+  
+  if (millis() >= gNextTime)
+  {
+    Joystick.setXAxis(number);
+//    if (digitalRead(A0) != 0)
+//    {
+//     Joystick.setXAxis(-127);
+//    }
+//    else
+//    {
+//     Joystick.setXAxis(0); 
+//    }
+
+//    gNextTime = millis() + gcAnalogDelta;
+//    testThrottleRudder(gCurrentStep - 37);
+//
+//    gNextTime = millis() + gcAnalogDelta;
+//    testXYAxis(gCurrentStep - (37 + 256));
+    
+    if (testAutoSendMode == false)
+    {
+      Joystick.sendState();
+    }
+    
+    gCurrentStep++;
+    if (gCurrentStep == (37 + 256 + 1024 + 128 + 510 + 28 + 360))
+    {
+      gNextTime = millis() + gcCycleDelta;
+      gCurrentStep = 0;
+    }
+  }
+}
+
+void receiveData(int byteCount){
+
+while(Wire.available()) {
+number = Wire.read();
+Serial.print("data received: ");
+Serial.println(number);
+
+if (number == 1){
+
+if (state == 0){
+digitalWrite(13, HIGH); // set the LED on
+state = 1;
+}
+else{
+digitalWrite(13, LOW); // set the LED off
+state = 0;
+}
+}
+}
+}
+
+// callback for sending data
+void sendData(){
+Wire.write(number);
+}
